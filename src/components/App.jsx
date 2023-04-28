2d92421d20eaa6eec7bcb47a43dbac74fb46bf27
unknown <ivan.ruskevych@gmail.com>
2023-04-28 05:31:32 +0300

    01-styling--Repeta

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
+import PageTitle from './PageTitle/PageTitle';
+import EventBoard from './EventBoard/EventBoard';
+import upcomingEvents from 'upcoming-events.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <PageTitle text="24th Core World Coalition Conference" />
+      <EventBoard events={upcomingEvents} />
+    </>
   );
-};
+}
