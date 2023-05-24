eaae5d38464c28d173ec719eb18caed3aca9cbd3
unknown <ivan.ruskevych@gmail.com>
2023-05-24 03:20:54 +0300

    my--05--1--Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import Dogs from 'pages/Dogs';
+import Home from 'pages/Home';
+import DogDetails from 'pages/DogDetails';
+import LayOut from 'components/LayOut';
+import { Gallery } from './Gallery';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React --- My practice ---
-    </div>
+    <Routes>
+      <Route path="/" element={<LayOut />}>
+        <Route index element={<Home />} />
+        <Route path="/dogs" element={<Dogs />} />
+        <Route path="/dogs/:dogId" element={<DogDetails />}>
+          <Route path="subbreeds" element={<div>subbreeds--page</div>} />
+          <Route path="gallery" element={<Gallery />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
