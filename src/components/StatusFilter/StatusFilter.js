dba9c97494bf553b2e26cf1dc5d133563ee36ee7
unknown <ivan.ruskevych@gmail.com>
2023-06-05 04:42:43 +0300

    my--06--Redux--textbook--store/state

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,26 @@
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from '../../redux/constants';
+
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css';
+import { getStatusFilter } from 'redux/selectors';
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  // const filter = useSelector(state => state.filters.status);// var 01
+  const filter = useSelector(getStatusFilter); // var 02
+
+  // console.log(filter === statusFilters.all);
+  // console.log(filter === statusFilters.active);
+  // console.log(filter === statusFilters.completed);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,10 +1,39 @@
 import { Task } from 'components/Task/Task';
 import css from './TaskList.module.css';
+import { useSelector } from 'react-redux';
+import { statusFilters } from 'redux/constants';
+import { getStatusFilter, getTasks } from 'redux/selectors';
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 export const TaskList = () => {
+  // Отримуємо масив завдань із стану Redux
+  // const tasks = useSelector(state => state.tasks); // var 01
+  // console.log(tasks);
+  const tasks = useSelector(getTasks); // var 02
+  console.log(getTasks);
+  console.log(tasks);
+
+  // Отримуємо значення фільтра із стану Redux
+  // const statusFilter = useSelector(state => state.filters.status); // var 01
+  // console.log(statusFilter);
+  const statusFilter = useSelector(getStatusFilter); // var 02
+
+  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
   return (
     <ul className={css.list}>
-      {[].map(task => (
+      {visibleTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
