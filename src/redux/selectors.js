44196006f03b3bee07304d8e53e05c045fbba9d5
unknown <ivan.ruskevych@gmail.com>
2023-06-14 01:55:48 +0300

    refactoring getVisibleTasks --> selectVisibleTasks

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,24 @@
-export const selectTasks = state => state.tasks.items;
+import { statusFilters } from './constants';
+
+export const selectTasks = state => {
+  // console.log('selectTasks :>>', state);
+  return state.tasks.items;
+};
 export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 
 export const selectStatusFilter = state => state.filters.status;
+
+export const selectVisibleTasks = state => {
+  const statusFilter = selectStatusFilter(state);
+  const tasks = selectTasks(state);
+
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
