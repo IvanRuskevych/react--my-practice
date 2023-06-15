3765cbd2b09287f1947b7f945dce3a338616a253
unknown <ivan.ruskevych@gmail.com>
2023-06-15 23:19:23 +0300

    my--07--1--async-Redux--textbook--task-manager

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://6487235cbeba629727901137.mockapi.io';
+axios.defaults.baseURL = 'https://648b6fad17f1536d65eaf4aa.mockapi.io';
+// 'https://62584f320c918296a49543e7.mockapi.io' // textbook link
 
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
